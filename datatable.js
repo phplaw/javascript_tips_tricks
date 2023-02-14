a6779e52ee4eb6b48cd3bf6627058180058277a1
Sonny Nguyen <phplaw@gmail.com>
2023-02-15 05:08:14 +0700

    Update datatable.js

--- datatable.js
+++ datatable.js
@@ -1,3 +1,5 @@
  // all about databtable.js
 allFilters = currentFilters.join('|').replace("&", "\\&").replace(/\s/g, "\\s");
 table.columns(columnIndex).search(allFilters, true).draw();
+// for working with export excel here is start document
+// https://datatables.net/reference/button/excelHtml5
