d30526270798987a0892142be22d8db798e78e28
Sonny Nguyen <phplaw@gmail.com>
2023-02-20 00:19:28 +0700

    Update jquery_tips.js

--- jquery_tips.js
+++ jquery_tips.js
@@ -3,3 +3,12 @@ $("#clickable a").click(function(e) {
    // Do something
    e.stopPropagation();
 });
+// get element after click
+
+$(document).click(function(event) {
+    //var target = $( event.target );
+    //filterList
+    //if ( target.is( "li" ) ) {
+    //    target.children().toggle();
+    //}
+});
