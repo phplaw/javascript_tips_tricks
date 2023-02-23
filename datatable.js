b1d305648ea52c84fd7816ad86783f6ba74d651c
Sonny Nguyen <phplaw@gmail.com>
2023-02-23 16:22:42 +0700

    Update datatable.js

--- datatable.js
+++ datatable.js
@@ -3,3 +3,11 @@ allFilters = currentFilters.join('|').replace("&", "\\&").replace(/\s/g, "\\s");
 table.columns(columnIndex).search(allFilters, true).draw();
 // for working with export excel here is start document
 // https://datatables.net/reference/button/excelHtml5
+
+// get unique data in columns 
+var unique_data = [];
+ datatables_instance.column(_index).data().unique().sort().each(function (d, j) {
+                if(d!=='') {
+                    unique_data.push(d);
+                }
+            });
