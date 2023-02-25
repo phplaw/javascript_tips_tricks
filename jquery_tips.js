3e78cdd6167eb00fd021c020e01f8791ff012762
Sonny Nguyen <phplaw@gmail.com>
2023-02-25 22:21:22 +0700

    Update jquery_tips.js

--- jquery_tips.js
+++ jquery_tips.js
@@ -12,3 +12,13 @@ $(document).click(function(event) {
     //    target.children().toggle();
     //}
 });
+
+// attributes selector 
+/*
+$("[attribute]")	Select all matched elements with the specified attribute.
+$("[attribute='value']")	Select all matched elements with the specified attribute and the specified value.
+$("[attribute^='value']")	Select all matched elements with the specified attribute and start with the specified value.
+$("[attribute$='value']")	Select all matched elements with the specified attribute and end with the specified value.
+$("[attribute*='value']")	Select all matched elements with the specified attribute and the value contains specified substring.
+$("[attribute!='value']")	Select all matched elements that do not contain the specified attribute with the specified value.
+*/
